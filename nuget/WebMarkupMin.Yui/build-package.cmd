2fb8fba870ff49b8467d96f1056fce8b429bcad5
Andrey Taritsyn <taritsyn@gmail.com>
2015-12-21 14:28:10 +0300

    Changed NuGet packages build scripts

--- nuget/WebMarkupMin.Yui/build-package.cmd
+++ nuget/WebMarkupMin.Yui/build-package.cmd
@@ -10,7 +10,7 @@ rmdir lib /Q/S
 %net40_msbuild% %project_source_dir%\%project_name%.Net40.csproj /p:Configuration=Release
 xcopy %project_source_dir%\bin\Release\%project_name%.dll lib\net40-client\
 
-%dnx_runtime% --appbase %project_source_dir% %dnx_package_manager% pack %project_source_dir% --framework net451 --configuration Release --out %project_artifacts_dir%
+%dnx_runtime% --appbase %project_source_dir% %dnx_package_manager% build %project_source_dir% --framework net451 --configuration Release --out %project_artifacts_dir%
 xcopy %project_artifacts_dir%\Release\net451\%project_name%.dll lib\net451\ /E
 xcopy %project_artifacts_dir%\Release\net451\%project_name%.xml lib\net451\ /E
 
