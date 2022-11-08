7c34a2eb79b100a73118244a23ad210425fc01c5
Taritsyn <taritsyn@gmail.com>
2022-11-08 20:37:04 +0300

    Reorganized a processing invalid code tests

--- test/WebMarkupMin.Tests/Html/Common/Parsing/ProcessingInvalidCodeTests.cs
+++ test/WebMarkupMin.Tests/Html/Common/Parsing/ProcessingInvalidCodeTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 using Xunit;
@@ -7,35 +8,193 @@ using WebMarkupMin.Core;
 
 namespace WebMarkupMin.Tests.Html.Common.Parsing
 {
-	public class ProcessingInvalidCodeTests
+	public class ProcessingInvalidCodeTests : IDisposable
 	{
 		private const string HTML_FILES_DIRECTORY_PATH = @"Files/html/";
 
+		private HtmlMinifier _minifier;
+
+
+		public ProcessingInvalidCodeTests()
+		{
+			_minifier = new HtmlMinifier(new HtmlMinificationSettings(true));
+		}
+
 
 		[Fact]
-		public void ProcessingInvalidCharactersInStartTagIsCorrect()
+		public void ProcessingInvalidCharactersInMetaStartTagIsCorrect()
 		{
 			// Arrange
-			var minifier = new HtmlMinifier(new HtmlMinificationSettings(true));
-
-			const string input1 = "<img src=\"/images/39.gif\" width=80\" height=\"60\">";
-			const string input2 = "<span class=\"b-form-input_size_l i-bem\"\">Some text...</span>";
-			const string input3 = "<meta name=\"Description\" content=\"Информационно-аналитический портал про " +
+			const string input = "<meta name=\"Description\" content=\"Информационно-аналитический портал про " +
 				"недвижимость и квартиры в Москве и РФ \"Хата.ру\" – Аналитика и цены на недвижимость онлайн, " +
 				"прогнозы, исследования, обзоры и статьи.\">"
 				;
-			const string input4 = "<img src=\"http://files.example.com/img/88900173ct.jpg\" width=\"100\" " +
+
+			// Act
+			IList<MinificationErrorInfo> errors = _minifier.Minify(input).Errors;
+
+			// Assert
+			Assert.Equal(1, errors.Count);
+			Assert.Equal("In the start tag <meta> found invalid characters.", errors[0].Message);
+			Assert.Equal(1, errors[0].LineNumber);
+			Assert.Equal(120, errors[0].ColumnNumber);
+			Assert.Equal(
+				"Line 1: … недвижимость и квартиры в Москве и РФ \"Хата.ру\" – Аналитика и цены на недвижимость онлайн, п…" + Environment.NewLine +
+				"--------------------------------------------------------^" + Environment.NewLine,
+				errors[0].SourceFragment
+			);
+		}
+
+		[Fact]
+		public void ProcessingInvalidCharactersInLinkStartTagIsCorrect()
+		{
+			// Arrange
+			const string input = "<link id=\"favicon\" rel=?\"shortcut icon\" type=?\"image/?png\" href=?\"#\">";
+
+			// Act
+			IList<MinificationErrorInfo> errors = _minifier.Minify(input).Errors;
+
+			// Assert
+			Assert.Equal(1, errors.Count);
+			Assert.Equal("In the start tag <link> found invalid characters.", errors[0].Message);
+			Assert.Equal(1, errors[0].LineNumber);
+			Assert.Equal(25, errors[0].ColumnNumber);
+			Assert.Equal(
+				"Line 1: <link id=\"favicon\" rel=?\"shortcut icon\" type=?\"image/?png\" href=?\"#\">" + Environment.NewLine +
+				"--------------------------------^" + Environment.NewLine,
+				errors[0].SourceFragment
+			);
+		}
+
+		[Fact]
+		public void ProcessingInvalidCharactersInHeaderStartTagIsCorrect()
+		{
+			// Arrange
+			const string input = "<header\"></header>";
+
+			// Act
+			IList<MinificationErrorInfo> errors = _minifier.Minify(input).Errors;
+
+			// Assert
+			Assert.Equal(1, errors.Count);
+			Assert.Equal("In the start tag <header> found invalid characters.", errors[0].Message);
+			Assert.Equal(1, errors[0].LineNumber);
+			Assert.Equal(8, errors[0].ColumnNumber);
+			Assert.Equal(
+				"Line 1: <header\"></header>" + Environment.NewLine +
+				"---------------^" + Environment.NewLine,
+				errors[0].SourceFragment
+			);
+		}
+
+		[Fact]
+		public void ProcessingInvalidCharactersInDivStartTagIsCorrect()
+		{
+			// Arrange
+			const string input = "<div class=\"row\">\n" +
+				"	<div class=\"span12>\n" +
+				"		Level 1 of column\n" +
+				"		<div class=\"row\">\n" +
+				"			<div class=\"span6\">Level 2</div>\n" +
+				"			<div class=\"span6\">Level 2</div>\n" +
+				"		</div>\n" +
+				"	</div>\n" +
+				"</div>"
+				;
+
+			// Act
+			IList<MinificationErrorInfo> errors = _minifier.Minify(input).Errors;
+
+			// Assert
+			Assert.Equal(1, errors.Count);
+			Assert.Equal("In the start tag <div> found invalid characters.", errors[0].Message);
+			Assert.Equal(4, errors[0].LineNumber);
+			Assert.Equal(18, errors[0].ColumnNumber);
+			Assert.Equal(
+				"Line 3:         Level 1 of column" + Environment.NewLine +
+				"Line 4:         <div class=\"row\">" + Environment.NewLine +
+				"-------------------------------^" + Environment.NewLine +
+				"Line 5:             <div class=\"span6\">Level 2</div>" + Environment.NewLine,
+				errors[0].SourceFragment
+			);
+
+		}
+
+		[Fact]
+		public void ProcessingInvalidCharactersInSpanStartTagIsCorrect()
+		{
+			// Arrange
+			const string input = "<span class=\"b-form-input_size_l i-bem\"\">Some text...</span>";
+
+			// Act
+			IList<MinificationErrorInfo> errors = _minifier.Minify(input).Errors;
+
+			// Assert
+			Assert.Equal(1, errors.Count);
+			Assert.Equal("In the start tag <span> found invalid characters.", errors[0].Message);
+			Assert.Equal(1, errors[0].LineNumber);
+			Assert.Equal(40, errors[0].ColumnNumber);
+			Assert.Equal(
+				"Line 1: <span class=\"b-form-input_size_l i-bem\"\">Some text...</span>" + Environment.NewLine +
+				"-----------------------------------------------^" + Environment.NewLine,
+				errors[0].SourceFragment
+			);
+		}
+
+		[Fact]
+		public void ProcessingInvalidCharactersInAnchorStartTagIsCorrect()
+		{
+			// Arrange
+			const string input1 = "<A HREF='/cgi-bin/user.cgi?user=O'Connor'>O'Connor</A>";
+			const string input2 = "<li>\n" +
+				"	<a data-bind=\"href:'@Url.Action(\"GetInvoices\", \"ListUsers\")'+'/?eMail='+ Login\">Invoices</a>\n" +
+				"</li>"
+				;
+
+			// Act
+			IList<MinificationErrorInfo> errors1 = _minifier.Minify(input1).Errors;
+			IList<MinificationErrorInfo> errors2 = _minifier.Minify(input2).Errors;
+
+			// Assert
+			Assert.Equal(1, errors1.Count);
+			Assert.Equal("In the start tag <A> found invalid characters.", errors1[0].Message);
+			Assert.Equal(1, errors1[0].LineNumber);
+			Assert.Equal(41, errors1[0].ColumnNumber);
+			Assert.Equal(
+				"Line 1: <A HREF='/cgi-bin/user.cgi?user=O'Connor'>O'Connor</A>" + Environment.NewLine +
+				"------------------------------------------------^" + Environment.NewLine,
+				errors1[0].SourceFragment
+			);
+
+			Assert.Equal(1, errors2.Count);
+			Assert.Equal("In the start tag <a> found invalid characters.", errors2[0].Message);
+			Assert.Equal(2, errors2[0].LineNumber);
+			Assert.Equal(46, errors2[0].ColumnNumber);
+			Assert.Equal(
+				"Line 1: <li>" + Environment.NewLine +
+				"Line 2:     <a data-bind=\"href:'@Url.Action(\"GetInvoices\", \"ListUsers\")'+'/?eMail='+ Login\">Invoices</a>" + Environment.NewLine +
+				"--------------------------------------------------------^" + Environment.NewLine +
+				"Line 3: </li>" + Environment.NewLine,
+				errors2[0].SourceFragment
+			);
+		}
+
+		[Fact]
+		public void ProcessingInvalidCharactersInImageStartTagIsCorrect()
+		{
+			// Arrange
+			const string input1 = "<img src=\"/images/39.gif\" width=80\" height=\"60\">";
+			const string input2 = "<img src=\"http://files.example.com/img/88900173ct.jpg\" width=\"100\" " +
 				"title=\"Программирование на COBOL, 14-е издание, I том (файл PDF) \" / " +
 				"alt=\"Программирование на COBOL, 14-е издание, I том (файл PDF)\" />"
 				;
-			const string input5 = "<A HREF='/cgi-bin/user.cgi?user=O'Connor'>O'Connor</A>";
-			const string input6 = "<noscript>\r" +
+			const string input3 = "<noscript>\r" +
 				"	<div>\r" +
 				"		<img src=\"//mk.tyndex.ru/watch/6740060\" style=\"position:absolute; left:-9999px;\" alt=\"\"\" />\r" +
 				"	</div>\r" +
 				"</noscript>"
 				;
-			const string input7 = "<table class='grayblock' style='width:100%'>\r\n" +
+			const string input4 = "<table class='grayblock' style='width:100%'>\r\n" +
 				"	<tbody>\r\n" +
 				"		<tr valign='middle'>\r\n" +
 				"			<td><a href='/r7165'><img width='55' border='0' height='55' " +
@@ -47,100 +206,136 @@ namespace WebMarkupMin.Tests.Html.Common.Parsing
 				"	</tbody>\r\n" +
 				"</table>"
 				;
-			const string input8 = "<li>\n" +
-				"	<a data-bind=\"href:'@Url.Action(\"GetInvoices\", \"ListUsers\")'+'/?eMail='+ Login\">Invoices</a>\n" +
-				"</li>"
-				;
-			const string input9 = "<header\"></header>";
-			const string input10 = "<table>\n" +
+
+			// Act
+			IList<MinificationErrorInfo> errors1 = _minifier.Minify(input1).Errors;
+			IList<MinificationErrorInfo> errors2 = _minifier.Minify(input2).Errors;
+			IList<MinificationErrorInfo> errors3 = _minifier.Minify(input3).Errors;
+			IList<MinificationErrorInfo> errors4 = _minifier.Minify(input4).Errors;
+
+			// Assert
+			Assert.Equal(1, errors1.Count);
+			Assert.Equal("In the start tag <img> found invalid characters.", errors1[0].Message);
+			Assert.Equal(1, errors1[0].LineNumber);
+			Assert.Equal(35, errors1[0].ColumnNumber);
+			Assert.Equal(
+				"Line 1: <img src=\"/images/39.gif\" width=80\" height=\"60\">" + Environment.NewLine +
+				"------------------------------------------^" + Environment.NewLine,
+				errors1[0].SourceFragment
+			);
+
+			Assert.Equal(1, errors2.Count);
+			Assert.Equal("In the start tag <img> found invalid characters.", errors2[0].Message);
+			Assert.Equal(1, errors2[0].LineNumber);
+			Assert.Equal(135, errors2[0].ColumnNumber);
+			Assert.Equal(
+				"Line 1: …ние на COBOL, 14-е издание, I том (файл PDF) \" / alt=\"Программирование на COBOL, 14-е издание…" + Environment.NewLine +
+				"--------------------------------------------------------^" + Environment.NewLine,
+				errors2[0].SourceFragment
+			);
+
+			Assert.Equal(1, errors3.Count);
+			Assert.Equal("In the start tag <img> found invalid characters.", errors3[0].Message);
+			Assert.Equal(3, errors3[0].LineNumber);
+			Assert.Equal(90, errors3[0].ColumnNumber);
+			Assert.Equal(
+				"Line 2:     <div>" + Environment.NewLine +
+				"Line 3:         <img src=\"//mk.tyndex.ru/watch/6740060\" style=\"position:absolute; left:-9999px;\" alt=\"\"\" />" + Environment.NewLine +
+				"-------------------------------------------------------------------------------------------------------^" + Environment.NewLine +
+				"Line 4:     </div>" + Environment.NewLine,
+				errors3[0].SourceFragment
+			);
+
+			Assert.Equal(1, errors4.Count);
+			Assert.Equal("In the start tag <img> found invalid characters.", errors4[0].Message);
+			Assert.Equal(4, errors4[0].LineNumber);
+			Assert.Equal(135, errors4[0].ColumnNumber);
+			Assert.Equal(
+				"Line 3: " + Environment.NewLine +
+				"Line 4: …му Общему собранию акционеров ОАО Рога и копыта' alt='Материалы к годовому Общему собранию ак…" + Environment.NewLine +
+				"--------------------------------------------------------^" + Environment.NewLine +
+				"Line 5: …та</a></td>" + Environment.NewLine,
+				errors4[0].SourceFragment
+			);
+		}
+
+		[Fact]
+		public void ProcessingInvalidCharactersInTableEndTagIsCorrect()
+		{
+			// Arrange
+			const string input = "<table>\n" +
 				"	<tr>\n" +
 				"		<td></td>\n" +
 				"	</tr>\n" +
 				"</table."
 				;
-			const string input11 = "<link id=\"favicon\" rel=?\"shortcut icon\" type=?\"image/?png\" href=?\"#\">";
-			string input12 = File.ReadAllText(
-				Path.Combine(HTML_FILES_DIRECTORY_PATH, "html-document-with-invalid-characters.html"));
 
 			// Act
-			IList<MinificationErrorInfo> errors1 = minifier.Minify(input1).Errors;
-			IList<MinificationErrorInfo> errors2 = minifier.Minify(input2).Errors;
-			IList<MinificationErrorInfo> errors3 = minifier.Minify(input3).Errors;
-			IList<MinificationErrorInfo> errors4 = minifier.Minify(input4).Errors;
-			IList<MinificationErrorInfo> errors5 = minifier.Minify(input5).Errors;
-			IList<MinificationErrorInfo> errors6 = minifier.Minify(input6).Errors;
-			IList<MinificationErrorInfo> errors7 = minifier.Minify(input7).Errors;
-			IList<MinificationErrorInfo> errors8 = minifier.Minify(input8).Errors;
-			IList<MinificationErrorInfo> errors9 = minifier.Minify(input9).Errors;
-			IList<MinificationErrorInfo> errors10 = minifier.Minify(input10).Errors;
-			IList<MinificationErrorInfo> errors11 = minifier.Minify(input11).Errors;
-			IList<MinificationErrorInfo> errors12 = minifier.Minify(input12).Errors;
+			IList<MinificationErrorInfo> errors = _minifier.Minify(input).Errors;
 
 			// Assert
-			Assert.Equal(1, errors1.Count);
-			Assert.Equal(1, errors1[0].LineNumber);
-			Assert.Equal(35, errors1[0].ColumnNumber);
-
-			Assert.Equal(1, errors2.Count);
-			Assert.Equal(1, errors2[0].LineNumber);
-			Assert.Equal(40, errors2[0].ColumnNumber);
-
-			Assert.Equal(1, errors3.Count);
-			Assert.Equal(1, errors3[0].LineNumber);
-			Assert.Equal(120, errors3[0].ColumnNumber);
-
-			Assert.Equal(1, errors4.Count);
-			Assert.Equal(1, errors4[0].LineNumber);
-			Assert.Equal(135, errors4[0].ColumnNumber);
-
-			Assert.Equal(1, errors5.Count);
-			Assert.Equal(1, errors5[0].LineNumber);
-			Assert.Equal(41, errors5[0].ColumnNumber);
-
-			Assert.Equal(1, errors6.Count);
-			Assert.Equal(3, errors6[0].LineNumber);
-			Assert.Equal(90, errors6[0].ColumnNumber);
-
-			Assert.Equal(1, errors7.Count);
-			Assert.Equal(4, errors7[0].LineNumber);
-			Assert.Equal(135, errors7[0].ColumnNumber);
-
-			Assert.Equal(1, errors8.Count);
-			Assert.Equal(2, errors8[0].LineNumber);
-			Assert.Equal(46, errors8[0].ColumnNumber);
-
-			Assert.Equal(1, errors9.Count);
-			Assert.Equal(1, errors9[0].LineNumber);
-			Assert.Equal(8, errors9[0].ColumnNumber);
-
-			Assert.Equal(1, errors10.Count);
-			Assert.Equal(5, errors10[0].LineNumber);
-			Assert.Equal(1, errors10[0].ColumnNumber);
-
-			Assert.Equal(1, errors11.Count);
-			Assert.Equal(1, errors11[0].LineNumber);
-			Assert.Equal(25, errors11[0].ColumnNumber);
-
-			Assert.Equal(1, errors12.Count);
-			Assert.Equal(1, errors12[0].LineNumber);
-			Assert.Equal(9, errors12[0].ColumnNumber);
+			Assert.Equal(1, errors.Count);
+			Assert.Equal("During parsing of HTML-code error has occurred.", errors[0].Message);
+			Assert.Equal(5, errors[0].LineNumber);
+			Assert.Equal(1, errors[0].ColumnNumber);
+			Assert.Equal(
+				"Line 4:     </tr>" + Environment.NewLine +
+				"Line 5: </table." + Environment.NewLine +
+				"--------^" + Environment.NewLine,
+				errors[0].SourceFragment
+			);
 		}
 
 		[Fact]
-		public void ProcessingInvalidHtmlCommentsIsCorrect()
+		public void ProcessingInvalidHtmlCommentIsCorrect()
 		{
 			// Arrange
-			var minifier = new HtmlMinifier(new HtmlMinificationSettings(true));
-
-			const string input1 = "<!-->";
+			const string input = "<!-->";
 
 			// Act
-			IList<MinificationErrorInfo> errors1 = minifier.Minify(input1).Errors;
+			IList<MinificationErrorInfo> errors = _minifier.Minify(input).Errors;
 
 			// Assert
-			Assert.Equal(1, errors1.Count);
-			Assert.Equal(1, errors1[0].LineNumber);
-			Assert.Equal(1, errors1[0].ColumnNumber);
+			Assert.Equal(1, errors.Count);
+			Assert.Equal("Comment is not closed.", errors[0].Message);
+			Assert.Equal(1, errors[0].LineNumber);
+			Assert.Equal(1, errors[0].ColumnNumber);
+			Assert.Equal(
+				"Line 1: <!-->" + Environment.NewLine +
+				"--------^" + Environment.NewLine,
+				errors[0].SourceFragment
+			);
 		}
+
+		[Fact]
+		public void ProcessingInvalidCodeGeneratedByFuzzerIsCorrect()
+		{
+			// Arrange
+			string input = File.ReadAllText(
+				Path.Combine(HTML_FILES_DIRECTORY_PATH, "html-document-with-invalid-characters.html"));
+
+			// Act
+			IList<MinificationErrorInfo> errors = _minifier.Minify(input).Errors;
+
+			// Assert
+			Assert.Equal(1, errors.Count);
+			Assert.Equal("In the start tag <C> found invalid characters.", errors[0].Message);
+			Assert.Equal(1, errors[0].LineNumber);
+			Assert.Equal(9, errors[0].ColumnNumber);
+			Assert.Equal(
+				"Line 1: <C�\u0003�tmh</`\u0018\u001al@\0OCTYPE \0O|�\0\0\u0010\0htmh</`�\u0001\0\0h<+`\u0006OCTFPE ElE Ml@\0\0\u0010|J\u0017h\0\u0010h1?html" + Environment.NewLine +
+				"----------------^" + Environment.NewLine,
+				errors[0].SourceFragment
+			);
+		}
+
+		#region IDisposable implementation
+
+		public void Dispose()
+		{
+			_minifier = null;
+		}
+
+		#endregion
 	}
 }
